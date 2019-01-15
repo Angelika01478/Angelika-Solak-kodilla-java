bf6898779fc61bb545983f8936f709e6e4df1459
Angelika01478 <angelika.solak@onet.eu>
2019-01-15 22:01:18 +0100

    modul6

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestMain.java
@@ -0,0 +1,8 @@
+package com.kodilla.testing;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Moduł 6. Wproowadzenie do testowania oprogramowania");
+        System.out.println("xxx");
+    }
+}
--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -1,4 +1,21 @@
 package com.kodilla;
 
 public class FirstClass {
+    public static void main(String[] args) {
+        int x=0;
+        int y=0;
+        while (x<5){
+            if(y<5){
+                x=x+1;
+                if(y<3){
+                    x=x-1;
+                }
+            }
+            y=y+2;
+            System.out.println(x + "" + y);
+            x=x+1;
+        }
+
+
+    }
 }
