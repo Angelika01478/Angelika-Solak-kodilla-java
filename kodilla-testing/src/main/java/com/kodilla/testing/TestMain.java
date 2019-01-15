bed993f49836259fc22b62e7999cfae7ecc2463e
Angelika01478 <angelika.solak@onet.eu>
2019-01-15 22:03:12 +0100

    Wprowadzenie do testowania oprogramowania

--- kodilla-testing/src/main/java/com/kodilla/testing/TestMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestMain.java
@@ -3,6 +3,5 @@ package com.kodilla.testing;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wproowadzenie do testowania oprogramowania");
-        System.out.println("xxx");
     }
 }
