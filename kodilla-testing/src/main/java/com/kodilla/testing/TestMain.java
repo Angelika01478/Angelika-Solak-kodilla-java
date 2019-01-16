e2858103bd815eb7ad11d16c4227a092dcd6a169
Angelika01478 <angelika.solak@onet.eu>
2019-01-16 22:11:05 +0100

    zadanie: pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestMain.java
@@ -1,7 +1,42 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println(" Moduł 6. Wproowadzenie do testowania oprogramowania ");
+       SimpleUser simpleUser = new SimpleUser("theFormUser");
+       String result =simpleUser.getUsername();
+
+       if (result.equals("theFormUser")) {
+           System.out.println("Test ok");
+       }else {
+           System.out.println("Error!");
+
+        }
+
+       Calculator calculator = new Calculator();
+       int addResult = calculator.add(10,20);
+
+       if (addResult==(10+20)){
+           System.out.println("Calculator adding test ok");
+       }else {
+           System.out.println("Calculator adding test error!");
+       }
+
+
+        int subtractResult = calculator.subtract(10,20);
+
+        if (subtractResult==(10-20)){
+            System.out.println("Calculator subtracting test ok");
+        }else {
+            System.out.println("Calculator subtracting test error!");
+        }
+
+
+
+
+
+
     }
 }
