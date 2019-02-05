6ff5f1d62d2d387df434d89e8a13ec835c0b009a
Angelika01478 <angelika.solak@onet.eu>
2019-02-05 20:04:39 +0100

    moduł 7-zadanie 7.2

---
+++ kodilla-stream/src/main/java/stream/immutable/ForumUser.java
@@ -0,0 +1,22 @@
+package stream.immutable;
+
+public final class ForumUser {
+
+    private String userName;
+    private String realName;
+
+    public ForumUser(String userName, String realName) {
+        this.userName = userName;
+        this.realName = realName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+
+}
