1d4da85fdce94af6027fcbf6d393e4c88e4ec3a5
Angelika01478 <angelika.solak@onet.eu>
2019-08-14 19:43:55 +0200

    Moduł 13 - zad2

---
+++ kodilla-spring-web-training/src/main/java/com/kodilla/kodilla/spring/web/training/KodillaSpringWebTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodilla.spring.web.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSpringWebTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaSpringWebTrainingApplication.class, args);
+    }
+
+}
