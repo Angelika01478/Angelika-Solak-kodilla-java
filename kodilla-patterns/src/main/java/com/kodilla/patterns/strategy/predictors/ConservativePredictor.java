0b00c87dab2324a4d52ceeca4ff9e8f3129a8116
Angelika01478 <angelika.solak@onet.eu>
2019-08-29 19:58:59 +0200

    Moduł 15 - zad3

--- kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/predictors/ConservativePredictor.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/predictors/ConservativePredictor.java
@@ -6,5 +6,6 @@ public class ConservativePredictor implements BuyPredictor {
     @Override
     public String predictWhatToBuy() {
         return "[Conservative predictor] Buy debentures of XYZ";
+
     }
 }
