5c1ba7fe493787ad2addd0cd3e4137643ee28720
Angelika01478 <angelika.solak@onet.eu>
2019-09-26 23:49:23 +0200

    Moduł 17 - zad4

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -10,11 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CompanyDaoTestSuite {
     @Autowired
     CompanyDao companyDao;
+
+    @Autowired
+    EmployeeDao employeeDao;
+
 
     @Test
     public void testSaveManyToMany(){
@@ -59,5 +65,52 @@ public class CompanyDaoTestSuite {
         }catch (Exception e){
 
         }
+    }
+
+
+    @Test
+    public void testNamedQueries(){
+        //Given
+        Employee janKowalski = new Employee("jan", "kowalski");
+        Employee janinaNowak = new Employee("janina", "nowak");
+        Employee lucyWilska = new Employee("lucy", "wilska");
+
+        Company company1 = new Company("samsung");
+        Company company2 = new Company("lenovo");
+        Company company3 = new Company("stateStreet");
+
+        company1.getEmployees().add(janKowalski);
+        company2.getEmployees().add(janinaNowak);
+        company1.getEmployees().add(janKowalski);
+        company3.getEmployees().add(lucyWilska);
+
+        janKowalski.getCompanies().add(company1);
+        janKowalski.getCompanies().add(company3);
+        janinaNowak.getCompanies().add(company2);
+        lucyWilska.getCompanies().add(company3);
+        //When
+        companyDao.save(company1);
+        int id1 = company1.getId();
+
+        companyDao.save(company2);
+        int id2 = company2.getId();
+
+        companyDao.save(company3);
+        int id3 = company3.getId();
+
+        List<Employee> byLastName =employeeDao.searchByLastName("kowalski");
+        List<Company> byKey =companyDao.searchByKey("sam");
+
+        Assert.assertEquals("kowalski", byLastName.get(0).getLastName());
+        Assert.assertEquals("samsung", byKey.get(0).getName());
+
+        try{
+            companyDao.deleteById(id1);
+            companyDao.deleteById(id2);
+            companyDao.deleteById(id3);
+        }catch (Exception e){
+
+        }
+
     }
 }
